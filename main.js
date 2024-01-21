35cb04e9587e83da13beb04abdab070036d92d93
farhansiddiq16 <157281829+farhansiddiq16@users.noreply.github.com>
2024-01-21 17:50:49 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+for (let i = 1; i < 11; i++) {
+    let ans = i * 3;
+    console.log("3", "x", i, "=", ans);
+}
+// or
+let num = 4;
+for (let i = 1; i < 11; i++) {
+    let ans = i * num;
+    console.log(num, "x", i, "=", ans);
+}
